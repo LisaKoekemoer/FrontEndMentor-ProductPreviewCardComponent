b363d699214e7ef3e08dfdbb035d2d7a62d50cc0
Alicia <alicia.koekemoer98@gmail.com>
2024-06-02 17:42:22 +0200

    Completed Product Preview Card Component Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import chanel from './images/image-product-desktop.jpg';
+import cart from './images/icon-cart.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <div className='product-card'>
+          <div className='chanel-img'>
+            <img src={chanel} alt=""/>
+          </div>
+          <div className='product-info'>
+              <p className='montserrat perfume'>PERFUME</p>         
+
+              <h2 className='fraunces gabrielle'>Gabrielle Essence Eau De Parfum</h2>
+
+              <p className='montserrat product-description'>A floral, solar and voluptuous interpretation composed by Olivier 
+                Polge, Perfumer-Creator for the House of CHANEL.</p>
+              <div className='product-price'>
+                <h2 className='fraunces discount'>$149.99</h2>
+                <p className='montserrat original-price'>$169.99</p> 
+              </div>
+
+              <button type="submit" className='montserrat cart' ><img src={cart} alt=""/> Add to Cart</button>
+          </div>
+       </div> 
+    </>
   );
 }
 
